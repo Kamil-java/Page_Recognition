7b6e9641129ec73c6b8c8d8a55f1c4a26b4794d7
KokosJava <kamil.slowik@vp.pl>
2021-07-13 18:18:54 +0200

    Initial commit

---
+++ pageRecognition/src/main/java/pl/bak/pageRecognition/PageRecognitionApplication.java
@@ -0,0 +1,13 @@
+package pl.bak.pageRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageRecognitionApplication.class, args);
+	}
+
+}
---
+++ pageRecognition/src/test/java/pl/bak/pageRecognition/PageRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bak.pageRecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
